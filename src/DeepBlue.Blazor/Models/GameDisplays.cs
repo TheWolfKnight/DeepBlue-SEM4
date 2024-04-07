5b83d275ff8b27ae1d1009fa20c4df3da54c4876
TheWolfKnight <philipalexknudsen@gmail.com>
2024-04-07 20:37:00 +0200

    small work

--- src/DeepBlue.Blazor/Models/GameDisplays.cs
+++ src/DeepBlue.Blazor/Models/GameDisplays.cs
@@ -1,7 +1,15 @@
 
+using System.ComponentModel;
+
 namespace DeepBlue.Blazor.Models;
 
 public class GameDisplay
 {
+  public int GameID { get; set; }
 
+  [DisplayName("Last Played")]
+  public DateTime LastPlayed { get; set; } = new DateTime();
+
+  [DisplayName("FEN")]
+  public string FENString { get; set; } = string.Empty;
 }
